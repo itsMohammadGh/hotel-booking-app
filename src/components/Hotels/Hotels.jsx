19b823a026ed95c3dcd2521f00c17ad7a5b02ef5
itsMohammadGh <smgh.4032@gmail.com>
2026-09-26 15:29:27 +0330

    UseHotel context has updated , single hotel view bugs fixed

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -3,7 +3,7 @@ import Loader from "../Loader/Loader";
 import { useHotel } from "../../context/HotelsProvider";
 
 function Hotels() {
-  const { isLoading, hotels } = useHotel();
+  const { isLoading, hotels, currentHotel } = useHotel();
 
   if (isLoading) return <Loader />;
   return (
@@ -15,7 +15,9 @@ function Hotels() {
             key={item.id}
             to={`/hotels/${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
           >
-            <div className="searchItem">
+            <div
+              className={`searchItem ${item.id === currentHotel?.id ? "current-hotel" : ""}`}
+            >
               <img src={item.thumbnail_url} alt={item.name} />
               <div className="searchItemDesc">
                 <p className="location">{item.smart_location}</p>
--- src/context/HotelsProvider.jsx
+++ src/context/HotelsProvider.jsx
@@ -1,20 +1,46 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import useFetch from "../Hooks/UseFetch";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const HotelContext = createContext();
+const BASE_URL = "http://localhost:5000/hotels";
 
 function HotelsProvider({ children }) {
+  const [currentHotel, setCurrentHotel] = useState({});
+  const [isLoadingCurrentHotel, setIsLoadingCurrentHotel] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
   const destination = searchParams.get("destination");
   const rooms = JSON.parse(searchParams.get("options"))?.room;
   const { data: hotels, isLoading } = useFetch(
-    "http://localhost:5000/hotels",
+    BASE_URL,
     `q=${destination || ""}&accommodates_gte=${rooms || 1}`,
   );
 
+  async function getHotel(id) {
+    setIsLoadingCurrentHotel(true);
+    try {
+      const { data } = await axios.get(`${BASE_URL}/${id}`);
+      setCurrentHotel(data);
+      setIsLoadingCurrentHotel(false);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoadingCurrentHotel(false);
+    }
+  }
+
   return (
-    <HotelContext.Provider value={{ isLoading, hotels }}>
+    <HotelContext.Provider
+      value={{
+        isLoading,
+        hotels,
+        getHotel,
+        isLoadingCurrentHotel,
+        currentHotel,
+      }}
+    >
       {children}
     </HotelContext.Provider>
   );
